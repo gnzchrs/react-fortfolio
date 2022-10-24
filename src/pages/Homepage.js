2b7d66235e1526a4a8c9eb32e953d6cd57bce389
gnzchrs <christophergonzaga63@gmail.com>
2022-10-24 17:55:31 +0800

    added darkmode functionality

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -24,37 +24,6 @@ const Homepage = (props) => {
           justifyContent: 'space-evenly',
           alignItems: 'center'
         }}>
-<<<<<<< Updated upstream
-        <div
-          style={{
-            display: 'flex',
-            flex: 1,
-            justifyContent: 'space-evenly',
-            alignItems: 'center'
-          }}>
-          <div>
-            <div data-aos="fade-down-right">
-              <Typography variant="h2">Hi! I'm Gonz.</Typography>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <Typography variant="h4" sx={{ marginRight: 1 }}>
-                I am a
-              </Typography>
-              <div style={{ fontSize: '2em' }}>
-                <Typewriter
-                  options={{
-                    strings: strings,
-                    autoStart: true,
-                    delay: 40,
-                    loop: true
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-          <div data-aos="fade-left" data-aos-duration={1000} class="avatar">
-            <Avatar alt="myavatar" src={myAvatar} sx={{ width: '320px', height: '320px' }} />
-=======
         <div>
           <div data-aos="fade-down-right">
             <Typography variant="h2">
@@ -75,7 +44,6 @@ const Homepage = (props) => {
                 }}
               />
             </Typography>
->>>>>>> Stashed changes
           </div>
         </div>
         <div data-aos="fade-left" data-aos-duration={1000} class="avatar">
