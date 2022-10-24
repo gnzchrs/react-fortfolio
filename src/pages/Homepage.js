2170cf419ea456a0d4fc328a3865349418333760
gnzchrs <christophergonzaga63@gmail.com>
2022-10-24 17:55:08 +0800

    added darkmode functionality

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,16 +8,23 @@ const Homepage = (props) => {
   const strings = ['Mobile Developer.', 'Software Engineer.', 'Programmer.']
 
   return (
-    <>
+    <div
+      id={props.id}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+        // backgroundColor: 'rgba(238,244,240,255)'
+      }}>
       <div
-        id={props.id}
         style={{
-          height: '100vh',
           display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'rgba(238,244,240,255)'
+          flex: 1,
+          justifyContent: 'space-evenly',
+          alignItems: 'center'
         }}>
+<<<<<<< Updated upstream
         <div
           style={{
             display: 'flex',
@@ -47,10 +54,35 @@ const Homepage = (props) => {
           </div>
           <div data-aos="fade-left" data-aos-duration={1000} class="avatar">
             <Avatar alt="myavatar" src={myAvatar} sx={{ width: '320px', height: '320px' }} />
+=======
+        <div>
+          <div data-aos="fade-down-right">
+            <Typography variant="h2">
+              <span style={{ color: 'red' }}>Hi!</span> I'm Gonz.
+            </Typography>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography variant="h4" sx={{ marginRight: 1 }}>
+              I am a
+            </Typography>
+            <Typography variant="h4" sx={{ color: 'red' }}>
+              <Typewriter
+                options={{
+                  strings: strings,
+                  autoStart: true,
+                  delay: 40,
+                  loop: true
+                }}
+              />
+            </Typography>
+>>>>>>> Stashed changes
           </div>
         </div>
+        <div data-aos="fade-left" data-aos-duration={1000} class="avatar">
+          <Avatar alt="myavatar" src={myAvatar} sx={{ width: '320px', height: '320px' }} />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 export default Homepage
